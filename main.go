f135c1e5b1e74c3f71aeb9c96737f09509b60cda
Mylan Connolly <mconnolly@iotrak.com>
2018-12-18 11:51:35 -0500

    Added support for -0 flag

--- main.go
+++ main.go
@@ -9,9 +9,15 @@ import (
 	"runtime"
 )
 
+const (
+	delimNewline = 1 << iota
+	delimNull
+)
+
 func main() {
 	jobs := flag.Int("j", runtime.NumCPU(), "Number of jobs to run; defaults to logical CPU core count.")
 	argFile := flag.String("a", "", "Use input-file as input source. Only one input source can be specified. In this case, stdin is discarded.")
+	nullDelim := flag.Bool("0", false, "Use NUL as delimiter. Normally input lines will end in \\n (newline). If they end in \\0 (NUL), then use this option. It is useful for processing arguments that may contain \\n (newline)")
 
 	flag.Parse()
 
@@ -19,13 +25,18 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Must specify program name to run")
 		os.Exit(1)
 	}
+	delimMode := delimNewline
+
+	if *nullDelim {
+		delimMode = delimNull
+	}
 	reader, err := getInput(*argFile)
 
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Problem getting input source:", err)
 		os.Exit(1)
 	}
-	q := newQueue(reader)
+	q := newQueue(reader, delimMode)
 	w := worker{queue: q, concurrency: *jobs, program: flag.Arg(0)}
 
 	w.runJobs()
--- queue.go
+++ queue.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"io"
 )
 
@@ -15,11 +16,18 @@ func (q queue) readLine() (string, bool) {
 	return str, more
 }
 
-func newQueue(reader io.Reader) queue {
+func newQueue(reader io.Reader, delimMode int) queue {
 	ch := make(chan string)
 
 	go func() {
 		scanner := bufio.NewScanner(reader)
+
+		switch delimMode {
+		case delimNewline:
+			scanner.Split(bufio.ScanLines)
+		case delimNull:
+			scanner.Split(scanNull)
+		}
 
 		for scanner.Scan() {
 			ch <- scanner.Text()
@@ -27,4 +35,28 @@ func newQueue(reader io.Reader) queue {
 		close(ch)
 	}()
 	return queue{ch: ch}
+}
+
+func scanNull(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := bytes.IndexByte(data, '\x00'); i >= 0 {
+		// We have a full null-terminated line.
+		return i + 1, dropCR(data[0:i]), nil
+	}
+	// If we're at EOF, we have a final, non-terminated line. Return it.
+	if atEOF {
+		return len(data), dropCR(data), nil
+	}
+	// Request more data.
+	return 0, nil, nil
+}
+
+// dropCR drops a terminal \r from the data.
+func dropCR(data []byte) []byte {
+	if len(data) > 0 && data[len(data)-1] == '\r' {
+		return data[0 : len(data)-1]
+	}
+	return data
 }
